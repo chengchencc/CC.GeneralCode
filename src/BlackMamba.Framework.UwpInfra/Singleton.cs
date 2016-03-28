82ea814bed21320074140bc3af6d2ecd33fcb3b7
chengchen <chengchen216@gmail.com>
2016-03-28 19:25:19 +0800

    add singleton

---
+++ src/BlackMamba.Framework.UwpInfra/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackMamba.Framework.UwpInfra
+{
+    public class Singleton<T> where T : new()
+    {
+        private static T _instance;
+        private static object _lock = new object();
+        public static T GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new T();
+                    }
+                }
+            }
+            return _instance;
+        }
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new T();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+    }
+
+}
